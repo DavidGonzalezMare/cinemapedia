63d56011da9e226a6f627b404cfd85daba79ed53
David González <d.gonzalezarco@edu.gva.es>
2025-03-19 10:59:02 +0100

    Creación Swiper

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,9 +1,10 @@
-
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
-
   static const name = 'home-screen';
 
   const HomeScreen({super.key});
@@ -11,7 +12,66 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    if (nowPlayingMovies.length == 0)
+      return Center(child: CircularProgressIndicator());
+
+    return Column(
+      children: [
+        CustomAppbar(),
+
+        MoviesSlideshow(movies: nowPlayingMovies),
+      ],
+    );
+  }
+}
+
+// Ya no utilizamos esto
+class ListaAntigua extends StatelessWidget {
+  const ListaAntigua({
+    super.key,
+    required this.nowPlayingMovies,
+  });
+
+  final List<Movie> nowPlayingMovies;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+          itemCount: nowPlayingMovies.length,
+          itemBuilder: (context, index) {
+            final movie = nowPlayingMovies[index];
+    
+            return ListTile(
+              title: Text(movie.title),
+            );
+          }),
     );
   }
 }
---
+++ lib/presentation/widgets/widgets.dart
@@ -0,0 +1,4 @@
+// Fichero barril para no exportar tanto
+
+export 'shared/custom_appbar.dart';
+export 'movies/movies_slideshow.dart';
